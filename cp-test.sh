c4e3a57a55e3ac3d0741fa537ec0c0965274d3ff
juergen.schwietering@teufel.local <juergen.schwietering@teufel.local>
2016-11-28 11:31:19 +0100

    python version update for test copy

--- cp-test.sh
+++ cp-test.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 python -m compileall pyfeld
-sudo cp -r pyfeld/* /usr/lib/python3.4/site-packages/pyfeld/
-sudo chmod -R 0644 /usr/lib/python3.4/site-packages/pyfeld/*
+sudo cp -r pyfeld/* /usr/lib/python3.5/site-packages/pyfeld/
+sudo chmod -R 0644 /usr/lib/python3.5/site-packages/pyfeld/*
